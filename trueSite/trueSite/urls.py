4c2c58c0b29910c1fea15e38661f5af69e71bad4
Alex <aleksandra.grid2012@gmail.com>
2019-12-24 20:02:08 +0300

    Created a homepage

--- trueSite/trueSite/urls.py
+++ trueSite/trueSite/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import homepage.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage.views.Home, name='Home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
