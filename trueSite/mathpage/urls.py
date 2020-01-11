ba613904c0fa0f6306e0bfa24be21142387af0a8
Alex <aleksandra.grid2012@gmail.com>
2020-01-11 12:18:49 +0300

    Ready to go on server

--- trueSite/mathpage/urls.py
+++ trueSite/mathpage/urls.py
@@ -4,5 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.math, name="math"),
-    path('razvitie-ponyatya-o-chisle.html/', views.tasks_per_theme, name="thematic_tasks"),
+    path('razvitie-ponyatya-o-chisle.html/', views.ponyatia_o_chisle, name="ponyatia_o_chisle"),
+    path('korni-stepeni-logarifmi.html/', views.korni, name="korni"),
+    path('priamie-i-ploskosti-v-prostranstve.html/', views.priamie, name="priamie"),
+    path('elementi-kombinatoriki.html/', views.kombinatorica, name="kombinatorica"),
+    path('coordinati-i-vectori.html/', views.coordinati, name="coordinati"),
+    path('osnovi-trigonometrii.html/', views.trigonometria, name="trigonometria"),
+    path('functions-ich-grafiki-i-svoistva.html/', views.functions, name="functions"),
+    path('mnogogranniki-i-kruglie-tela.html/', views.mnogogranniki, name="mnogogranniki"),
+    path('nachala-matanaliza.html/', views.matan, name="matan"),
+    path('integral-i-ego-primenenie.html/', views.integral, name="integral"),
+    path('elementi-teorii-veroyatnostey-i-matstat.html/', views.teorver, name="teorver"),
+    path('uravnenia-i-neravenstva.html/', views.neravenstva, name="neravenstva"),
 ]
