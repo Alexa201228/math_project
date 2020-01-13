93a966a61eac574e9d65839ed3f8f75e7be93243
Alex <aleksandra.grid2012@gmail.com>
2020-01-13 15:59:44 +0300

    Need to add stuff on pages

--- trueSite/homework/models.py
+++ trueSite/homework/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class homework(models.Model):
     title = models.CharField(max_length=200, null=True)
+    theme = models.CharField(max_length=300, null=True)
     image = models.ImageField(upload_to='homework_images', blank=True, null=True)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+
 
     def __str__(self):
         return self.title
