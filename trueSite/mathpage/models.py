a3bb90d4176d8f92d2df559c32b8e67696f2950b
Alex <aleksandra.grid2012@gmail.com>
2020-01-30 20:24:16 +0300

    fixing bags

--- trueSite/mathpage/models.py
+++ trueSite/mathpage/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class MathPage(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     theme = models.CharField(max_length=100, null=True)
-    image = models.ImageField(upload_to='math_images/', blank=True, default=None, null=True)
+    image = models.ImageField(upload_to='math_images/', blank=True, null=True)
     description = models.TextField(blank=True, default='')
     solution = models.TextField(blank=True, default='')
-    image_solution = models.ImageField(upload_to='math_images/', blank=True, default=None, null=True)
+    image_solution = models.ImageField(upload_to='math_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title + " " + self.theme
