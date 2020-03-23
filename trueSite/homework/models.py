79475c38fd3c57866ef2beeff96990f58cc09d6c
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-03-23 08:20:31 +0300

    updating homework page

--- trueSite/homework/models.py
+++ trueSite/homework/models.py
@@ -5,6 +5,7 @@ class homework(models.Model):
     theme = models.CharField(max_length=300, null=True)
     image = models.ImageField(upload_to='homework_images', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    homework_file = models.FileField(blank=True)
 
 
     def __str__(self):
