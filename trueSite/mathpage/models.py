8ba066b26f2cbe102c6b81764fc7bf19e9c6dbee
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-07-18 10:37:12 +0300

    trying new features

--- trueSite/mathpage/models.py
+++ trueSite/mathpage/models.py
@@ -14,4 +14,4 @@ class MathPage(models.Model):
         return self.title + " " + self.theme
 
     def get_absolute_url(self):
-        return reverse('category_theme', args=[self.theme, self.slug])
+        return reverse('math:category_theme', args=[self.theme, self.slug])
