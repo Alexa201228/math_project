da4f0ba2df664627e77456ee3c2de2e65742eb6f
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-07-19 10:05:18 +0300

    creating proper themes section

--- trueSite/metodics/views.py
+++ trueSite/metodics/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotInteger
 from .models import MetodPage
 
 def metod(request):
-    metods = MetodPage.objects
-    return render(request, 'metodics/metod.html', {'metods': metods})
+    metods = MetodPage.objects.all()
+    paginator = Paginator(metods, 10)
+    page = request.GET.get('page')
+    try:
+        metod = paginator.page(page)
+    except PageNotInteger:
+        metod = paginator.page(1)
+    except EmptyPage:
+        metod = paginator.page(paginator.num_pages)
+    return render(request, 'metodics/metod.html', {'metods': metods, 'page': page})
