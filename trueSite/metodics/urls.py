f83c72b245dea8491e204cb0922bd6d4ece00b9f
Alexa <aleksandra.grid2012@gmail.com>
2020-08-16 17:35:38 +0300

    create search engine

--- trueSite/metodics/urls.py
+++ trueSite/metodics/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.metod, name="metod")
+    path('', views.metod, name="metod"),
+    path('search/', views.metod_search, name='metod_search'),
+    
 ]
