f2c45991437f1ebeb5da14e08f21c4ae344c8df8
Alexa <aleksandra.grid2012@gmail.com>
2020-08-16 17:20:44 +0300

    create search engine

--- trueSite/metodics/models.py
+++ trueSite/metodics/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
+
 class MetodPage(models.Model):
     title = models.TextField(max_length=300)
     text_file = models.FileField(upload_to='metod/')
+    objects = models.Manager()
+
 
     def __str__(self):
         return self.title
--- trueSite/metodics/views.py
+++ trueSite/metodics/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import MetodPage
+from .forms import SearchForm
+from django.contrib.postgres.search import SearchVector
 
 def metod(request):
     metods = MetodPage.objects.all()
@@ -13,3 +15,19 @@ def metod(request):
     except EmptyPage:
         metod = paginator.page(paginator.num_pages)
     return render(request, 'metodics/metod.html', {'page': page, 'metods': metod})
+
+
+def metod_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = MetodPage.objects.annotate(search=SearchVector('title', 'body')).filter(search=query)
+
+    return render(request, 'metodics/search.html',
+                    {'form': form,
+                    'query':query,
+                    'results':results})
