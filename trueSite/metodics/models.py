6a67f1047dd971b7b37c2577c71171e439d83597
AlexMath <aleksandra.grid2012@gmail.com>
2020-08-18 09:56:32 +0000

    models change

--- trueSite/metodics/models.py
+++ trueSite/metodics/models.py
@@ -6,10 +6,7 @@ class MetodPage(models.Model):
     title = models.TextField(max_length=300)
     text_file = models.FileField(upload_to='metod/')
     objects = models.Manager()
-<<<<<<< HEAD
 
-=======
->>>>>>> f15ca1ede7f01b541aba9bfd8e479203263b739a
 
     def __str__(self):
         return self.title
