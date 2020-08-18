e09c74ce6705cc9f25141feed05b097dff357dff
AlexMath <aleksandra.grid2012@gmail.com>
2020-08-18 10:10:19 +0000

    aaaa

--- trueSite/metodics/models.py
+++ trueSite/metodics/models.py
@@ -5,10 +5,7 @@ from django.db import models
 class MetodPage(models.Model):
     title = models.TextField(max_length=300)
     text_file = models.FileField(upload_to='metod/')
-<<<<<<< HEAD
     objects = models.Manager()
-=======
->>>>>>> 65f6722cdc04da11cd40413f298e3d2e7b267558
 
 
     def __str__(self):
