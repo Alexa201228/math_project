53c13ecd5fcc608ea678c2a2206b544b25a97674
Alexa <aleksandra.grid2012@gmail.com>
2020-08-25 18:34:07 +0300

    make admin site better

--- trueSite/mathpage/admin.py
+++ trueSite/mathpage/admin.py
@@ -8,10 +8,7 @@ class MathAdmin(admin.ModelAdmin):
     list_filter = ('theme', )
     list_per_page = 10
     autocomplete_fields = ['theme']
+    search_fields = ('theme','title')
 
-class ThemeAdmin(admin.ModelAdmin):
-    search_fields = ('theme',)
-
-admin.site.register(MathPage, ThemeAdmin)
 
 
