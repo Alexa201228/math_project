8564895ca2792b7e41d7068af06ec5cf7db4c0a7
Alexa <aleksandra.grid2012@gmail.com>
2020-08-25 19:09:21 +0300

    make admin site better

--- trueSite/mathpage/models.py
+++ trueSite/mathpage/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 class MathPage(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
-    theme = models.ForeignKey('self')
+    theme = models.ForeignKey('self', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='math_images/', blank=True, null=True)
     description = models.TextField(blank=True, default='')
     solution = models.TextField(blank=True, default='')
