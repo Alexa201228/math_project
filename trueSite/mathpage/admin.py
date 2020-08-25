5531ed3001abf424c42e5160ec1c20023fec8dd6
Alexa <aleksandra.grid2012@gmail.com>
2020-08-25 19:35:59 +0300

    make admin site better

--- trueSite/mathpage/admin.py
+++ trueSite/mathpage/admin.py
@@ -3,7 +3,7 @@ from .models import MathPage, Themes
 
 @admin.register(MathPage)
 class MathAdmin(admin.ModelAdmin):
-    list_display = ('title', 'theme', 'slug')
+    list_display = ('title', 'slug')
     prepopulated_fields = {'slug': ('theme',)}
     list_filter = ('theme', )
     list_per_page = 10
