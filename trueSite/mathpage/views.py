21379eefb79c7125362572c6d24462eaac61ec04
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-08-26 14:06:31 +0300

    adding autocomplete in admin

--- trueSite/mathpage/views.py
+++ trueSite/mathpage/views.py
@@ -11,6 +11,29 @@ class ThemeAutocomplete(autocomplete.Select2QuerySetView):
         if self.q:
             qs = qs.filter(theme__istartswith=self.q)
         return qs
+    def get_create_option(self, context, q):
+        """Form the correct create_option to append to results."""
+        create_option = []
+        display_create_option = False
+        if self.create_field and q:
+            page_obj = context.get('page_obj', None)
+            if page_obj is None or page_obj.number == 1:
+                display_create_option = True
+
+            # Don't offer to create a new option if a
+            # case-insensitive) identical one already exists
+            existing_options = (self.get_result_label(result).lower()
+                                for result in context['object_list'])
+            if q.lower() in existing_options:
+                display_create_option = False
+
+        if display_create_option and self.has_add_permission(self.request):
+            create_option = [{
+                'id': q,
+                'text': _('Create "%(new_value)s"') % {'new_value': q},
+                'create_id': True,
+            }]
+        return create_option
 
 def math(request):
     mathtasks = MathPage.objects.all()
