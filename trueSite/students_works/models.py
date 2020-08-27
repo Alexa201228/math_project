d7221105f1265c3345c078d78cb1928b1d3f3d1f
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-08-27 14:35:59 +0300

    continue improving site

--- trueSite/students_works/models.py
+++ trueSite/students_works/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class StudentsWorks(models.Model):
     presentation = models.FileField(upload_to='presentations/')
+    source = models.URLField()
 
     def __str__(self):
         return str(self.presentation)[14:]
