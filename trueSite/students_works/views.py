439696596dea0d259086cfc70e6ef32fd191cea4
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-08-27 15:38:25 +0300

    continue improving site

--- trueSite/students_works/views.py
+++ trueSite/students_works/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import StudentsWorks
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def students(request):
-    studworks = StudentsWorks.objects
-    return render(request, 'students/works.html', {'studworks': studworks})
+    studworks = StudentsWorks.objects.all()
+    paginator = Paginator(studworks, 10)
+    page = request.GET.get('page')
+    try:
+        studw = paginator.page(page)
+    except PageNotAnInteger:
+        studw = paginator.page(1)
+    except EmptyPage:
+        studw = paginator.page(paginator.num_pages)
+    return render(request, 'students/works.html', {'page': page,'studworks': studw})
