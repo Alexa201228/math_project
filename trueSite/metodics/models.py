53adfd619928920753792d0f4e217b56a0d57d10
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-08-30 09:05:58 +0300

    add images on metod page

--- trueSite/metodics/models.py
+++ trueSite/metodics/models.py
@@ -6,6 +6,7 @@ class MetodPage(models.Model):
     title = models.TextField(max_length=300)
     text_file = models.FileField(upload_to='metod/')
     objects = models.Manager()
+    description = models.ImageField(blank=True, null=True, upload_to='metod_images/')
 
 
     def __str__(self):
