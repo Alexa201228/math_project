087a53a0f11be8f1b55df99a248970a6105b5168
Alexa <aleksandra.grid2012@gmail.com>
2020-08-30 18:58:43 +0300

    adding fields in student works

--- trueSite/students_works/models.py
+++ trueSite/students_works/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 class StudentsWorks(models.Model):
-    presentation = models.FileField(upload_to='presentations/')
+    presentation = models.FileField(upload_to='presentations/', blank=True, null=True)
+    name = models.CharField(max_length=300, blank=True, null=True)
     source = models.URLField()
 
     def __str__(self):
         return str(self.presentation)[14:]
 
     def title(self):
+        if self.name:
+            return self.name
         return str(self.presentation)[14:len(str(self.presentation)) - 5]
 
