6e6fdf74e02e2e7986ec811ab2cbb7fa2597bd1f
Alexa <aleksandra.grid2012@gmail.com>
2020-08-31 19:03:56 +0300

    creating school app

--- trueSite/School/models.py
+++ trueSite/School/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+
+
+class SchoolPage(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    theme = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='school_images/', blank=True, null=True)
+    description = models.TextField(blank=True, default='')
+    fileSolution = models.FileField(blank=True, upload_to='school_materials/')
+    slug = models.SlugField(max_length=250, default='')
+    image_solution = models.ImageField(upload_to='school_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title + " " + self.theme
+
+    def get_absolute_url(self):
+        return reverse('category', args=[self.theme, self.slug])
 
 # Create your models here.
