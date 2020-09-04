b12b2b71e528b36d97cbb18e7b03f4dd43af5d26
Alexa <aleksandra.grid2012@gmail.com>
2020-09-04 18:37:35 +0300

    adding sitemaps on site

--- trueSite/homework/models.py
+++ trueSite/homework/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class homework(models.Model):
     title = models.CharField(max_length=200, null=True)
@@ -6,7 +7,7 @@ class homework(models.Model):
     image = models.ImageField(upload_to='homework_images', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     homework_file = models.FileField(blank=True)
-
+    created = models.DateTimeField(auto_now_add=True, default=timezone.now, auto_now=True)
 
     def __str__(self):
         return self.title
--- trueSite/homework/urls.py
+++ trueSite/homework/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-
+from django.contrib.sitemaps.views import sitemap
+from homework.sitemaps import HomeworkSitemap
 from . import views
+
+sitemaps = {
+    'homeworks': HomeworkSitemap,
+}
 
 urlpatterns = [
     path('', views.homeworks, name="homework"),
     path('<int:task_id>/', views.task_details, name='task'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+    name='django.contrib.sitemaps.view.sitemap'),
 ]
