32654cfa76b848a4c7d3a1d6246f952a845720bf
Alexa <aleksandra.grid2012@gmail.com>
2020-09-04 19:55:38 +0300

    adding sitemaps on site

--- trueSite/trueSite/urls.py
+++ trueSite/trueSite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from mathpage..sitemaps import MathSitemap
+from mathpage.sitemaps import MathSitemap
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
