e473b95305a1210d537e99bf22ba013557fec00f
Alexa <aleksandra.grid2012@gmail.com>
2020-09-04 20:24:01 +0300

    adding sitemaps on site

--- trueSite/mathpage/sitemaps.py
+++ trueSite/mathpage/sitemaps.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from .models import MathPage
-from django.core.urls import reverse
+from django.urls import reverse
 
 class MathSitemap(Sitemap):
     changefreq = 'daily'
