41c8b08abc7711821b571283ed632b5577c3a1c7
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-09-06 10:19:17 +0300

    add pagination on schoolpage

--- trueSite/School/views.py
+++ trueSite/School/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import SchoolPage
 
 def schoolMath(request):
     tasks = SchoolPage.objects.all().order_by('title')
-    return render(request, 'School/mainSchoolPage.html', {'tasks': tasks})
+    paginator = Paginator(tasks, 10)
+    page = request.GET.get('page')
+    try:
+        task = paginator.page(page)
+    except PageNotAnInteger:
+        task = paginator.page(1)
+    except EmptyPage:
+        task = paginator.page(paginator.num_pages)
+    return render(request, 'School/mainSchoolPage.html', {'page': page, 'tasks': task})
 
 def category(request, theme):
     math_theme = SchoolPage.objects.filter(slug=theme)
     all_themes = SchoolPage.objects.all()
-    return render(request, 'School/category.html', {'tasks': math_theme, 'themes': all_themes})
+    paginator = Paginator(math_theme, 10)
+    page = request.GET.get('page')
+    try:
+        math_thema = paginator.page(page)
+    except PageNotAnInteger:
+        math_thema = paginator.page(1)
+    except EmptyPage:
+        math_thema = paginator.page(paginator.num_pages)
+    return render(request, 'School/category.html', {'page': page, 'tasks': math_thema, 'themes': all_themes})
 
 # Create your views here.
