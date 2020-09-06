f976bf4644bac0f405df027580ac19a024b74f87
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-09-06 09:03:47 +0300

    add pagination on mathpage

--- trueSite/mathpage/views.py
+++ trueSite/mathpage/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import MathPage
 
 def math(request):
     mathtasks = MathPage.objects.all()
-    return render(request, 'math/mathematics.html', {'mathtasks': mathtasks})
+    paginator = Paginator(mathtasks, 10)
+    page = request.GET.get('page')
+    try:
+        exercise = paginator.page(page)
+    except EmptyPage:
+        exercise = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        exercise = paginator.page(1)
+    return render(request, 'math/mathematics.html', {'page': page, 'mathtasks': exercise})
 
 def categoryTheme(request, theme):
     math_theme = MathPage.objects.filter(slug=theme)
     all_themes = MathPage.objects.all()
-    return render(request, 'math/categories.html', {'math_tasks': math_theme, 'themes': all_themes})
+    paginator = Paginator(math_theme, 10)
+    page = request.GET.get('page')
+    try:
+        math_thema = paginator.page(page)
+    except PageNotAnInteger:
+        math_thema = paginator.page(1)
+    except EmptyPage:
+        math_thema = paginator.page(paginator.num_pages)
+    return render(request, 'math/categories.html', {'page': page, 'math_tasks': math_thema, 'themes': all_themes})
     
