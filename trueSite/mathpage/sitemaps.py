c51b1e9f60e3e33ba49e6fdfdec74c4c729f92b4
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-09-06 12:54:06 +0300

    add sitemap on mathpage

--- trueSite/mathpage/sitemaps.py
+++ trueSite/mathpage/sitemaps.py
@@ -1,13 +1,18 @@
 from django.contrib.sitemaps import Sitemap
 from .models import MathPage
 from django.urls import reverse
+from mathpage.urls import urlpatterns as mathPatterns
 
 class MathSitemap(Sitemap):
     changefreq = 'daily'
     priority = 0.9
 
     def items(self):
-        return ['math']
+        obj = []
+        for pattern in mathPatterns:
+            obj.append(pattern.name)
+        return obj
+
 
     def location(self, item):
         return reverse(item)
