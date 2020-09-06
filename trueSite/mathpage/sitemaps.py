81f3cd17077ad8ef9a096c5b3f26885b8305f648
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-09-06 11:55:49 +0300

    add sitemap on mathpage

--- trueSite/mathpage/sitemaps.py
+++ trueSite/mathpage/sitemaps.py
@@ -7,4 +7,7 @@ class MathSitemap(Sitemap):
     priority = 0.9
 
     def items(self):
-        return ['mathpage']
+        return ['math']
+
+    def location(self, item):
+        return reverse(item)
