5923e7392ad5a418370ae8cfd2a6c17402e185e1
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-11-25 09:26:21 +0300

    change presentations page

--- trueSite/students_works/models.py
+++ trueSite/students_works/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class StudentsWorks(models.Model):
     presentation = models.FileField(upload_to='presentations/', blank=True, null=True)
     name = models.CharField(max_length=300, blank=True, null=True)
-    source = models.URLField()
+    source = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return str(self.presentation)[14:]
