99b3971f5e21cf854738ce707ee6f5b3db6582f2
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-12-26 14:07:07 +0300

    add blog app

--- trueSite/trueSite/settings.py
+++ trueSite/trueSite/settings.py
@@ -136,8 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'trueSite/static/'),
     os.path.join(BASE_DIR, 'blog/static/'),
+    os.path.join(BASE_DIR, 'trueSite/static/'),
+    
 ]
 
 STATIC_URL = '/static/'
