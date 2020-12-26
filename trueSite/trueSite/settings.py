7c623491b1b5b70ca3e03d0d608ebb5770a898ef
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-12-26 13:46:46 +0300

    add blog app

--- trueSite/trueSite/settings.py
+++ trueSite/trueSite/settings.py
@@ -140,7 +140,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'blog/static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
