c5f4e49656bc72fb7cf6548b8f4d8c2915c978b3
Alexa201228 <aleksandra.grid2012@gmail.com>
2020-12-27 11:52:17 +0300

    add blog to google

--- trueSite/blog/sitemaps.py
+++ trueSite/blog/sitemaps.py
@@ -9,4 +9,4 @@ class PostSitemap(Sitemap):
         return Post.objects.all()
 
     def location(self, item):
-        return '/blog/' + item.title + " " + item.body
+        return '/blog/' + item.title
