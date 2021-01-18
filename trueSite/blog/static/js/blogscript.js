1edcb2bd70351f0d34e4bf36b5f6df8b41611b5e
Alexa201228 <aleksandra.grid2012@gmail.com>
2021-01-18 11:09:01 +0300

    make return-to-top button

--- trueSite/blog/static/js/blogscript.js
+++ trueSite/blog/static/js/blogscript.js
@@ -38,7 +38,7 @@ function textWithPictures(text){
             .replace(/^### (.*$)/gim, '<h3>$1</h3>')
 		    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
 		    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-		    .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+		    .replace(/^\\qt (.*$)/gim, '<blockquote>$1</blockquote>')
 		    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
 		    .replace(/\*(.*)\*/gim, '<i>$1</i>')
 		    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
