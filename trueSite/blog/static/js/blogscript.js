a1774ffe34c2259f28619e293ed6f960a42e4ae1
Alexa201228 <aleksandra.grid2012@gmail.com>
2021-01-18 15:30:44 +0300

    adding custom markdown

--- trueSite/blog/static/js/blogscript.js
+++ trueSite/blog/static/js/blogscript.js
@@ -40,7 +40,7 @@ var temp = document.getElementsByTagName('p');
             .replace(/^### (.*$)/gim, '<h3>$1</h3>')
 		    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
 		    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-		    .replace(/^<qt> (.*$)/gim, '<q>$1</q>')
+		    .replace(/^\<qt>(.*$)\<\qt>/gim, '<q>$1</q>')
 		    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
 		    .replace(/\*(.*)\*/gim, '<i>$1</i>')
 		    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
